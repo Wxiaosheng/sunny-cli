88cb36429f87e6de5fbc6fb467ef8fadb8a8770a
xiaosheng <287915338@qq.com>
2022-11-26 16:46:36 +0800

    :sparkles: add checkVersion checkNodeVersion

---
+++ packages/core/core/lib/const.js
@@ -0,0 +1,6 @@
+// 默认最小的 Node 版本
+const DEFAULT_NODE_VERSION = '16.0.1'
+
+module.exports = {
+  DEFAULT_NODE_VERSION
+}
--- packages/core/core/lib/index.js
+++ packages/core/core/lib/index.js
@@ -1,3 +1,33 @@
-#!/usr/bin/env node
+const semver = require('semver')
+const log = require('@sunny-cli/log')
+const pkg = require('../package.json')
+const CONST = require('./const')
 
-console.log("sunny-cli starting...")
+const checkCliVersion = () => {
+  console.log('cli version', pkg.version)
+}
+
+const checkNodeVersion = () => {
+  const nodeVersion = process.version
+  if (semver.lt(nodeVersion, CONST.DEFAULT_NODE_VERSION)) {
+    throw new Error(`您的 Node 版本太低，请升级为 v${CONST.DEFAULT_NODE_VERSION} 及以上版本！！！`)
+  }
+}
+
+// 1、执行准备
+const preExce = () => {
+  try {
+    checkCliVersion()
+    checkNodeVersion()
+  } catch (error) {
+    log.error(error.message)
+  }
+}
+
+const cli = () => {
+  
+  preExce()
+
+}
+
+module.exports = cli
