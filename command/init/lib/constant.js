4003ae819b12af3661a214b0fb522da77b6e72d5
xiaosheng <287915338@qq.com>
2022-12-10 17:22:06 +0800

    :sparkles: 组件名称\版本号验证

---
+++ command/init/lib/constant.js
@@ -0,0 +1,8 @@
+
+const TYPE_PROJECT = 'project'
+const TYPE_COMPONENT = 'component'
+
+module.exports = {
+  TYPE_PROJECT,
+  TYPE_COMPONENT
+}
--- command/init/lib/index.js
+++ command/init/lib/index.js
@@ -1,7 +1,10 @@
 const inquirer = require('inquirer')
+const semver = require('semver')
 const fse = require('fs-extra')
+const log = require('@sunny-cli/log')
 const Command = require('@sunny-cli/command')
 const { isEmptyDir } = require('@sunny-cli/utils')
+const { TYPE_PROJECT, TYPE_COMPONENT } = require('./constant')
 
 class InitCommand extends Command {
     init () {
@@ -24,18 +27,63 @@ class InitCommand extends Command {
         }
         // 2、是否启动强制更新
         // 3、获取用户初始化信息
-        const questions = [
+        const { projectType } = await inquirer.prompt([
             {
                 type: 'list',
-                name: 'projectName',
+                name: 'projectType',
                 message: '请选择要创建的项目类型？',
-                default: 'project',
+                default: TYPE_PROJECT,
                 choices: [
-                    { name: '项目', value: 'project' },
-                    { name: '组件', value: 'component' }
+                    { name: '项目', value: TYPE_PROJECT },
+                    { name: '组件', value: TYPE_COMPONENT }
                 ]
             }
-        ]
+        ])
+        log.verbose('您已选择创建 ', projectType)
+        let questions
+        if (projectType === TYPE_PROJECT) {
+            questions = [{
+                type: 'input',
+                name: 'projectName',
+                default: '',
+                message: '请输入要创建的项目名称：',
+                validate: function(v) {
+                    // 1、首字符 必须为英文字符
+                    // 2、尾字符 必须为英文或数字，不能为字符
+                    // 3、字符仅允许 "-_"
+                    const done = this.async();
+                    setTimeout(function() {
+                        if (!/^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v)) {
+                            done('您输入的名称不合法！！！');
+                            return;
+                        }
+                        done(null, true);
+                    }, 0);
+                },
+                filter: (v) => {
+                    return v
+                }
+            },{
+                type: 'input',
+                name: 'projectVersion',
+                default: '',
+                message: '请输入要创建的项目版本号：',
+                validate: function (v) {
+                    const done = this.async();
+                    setTimeout(function() {
+                        if (!!!semver.valid(v)) {
+                            done('您输入的版本号不合法！！！');
+                            return;
+                        }
+                        done(null, true);
+                    }, 0);
+                    return 
+                }
+            }]
+        } else if (projectType === TYPE_COMPONENT) {
+            questions.push()
+        }
+        
         const info = await inquirer.prompt(questions)
         console.log('info', info)
     }
