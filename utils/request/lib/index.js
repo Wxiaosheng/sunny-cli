7071b608b8aeb88477fb2937eec7a3efa344a3fc
xiaosheng <287915338@qq.com>
2022-12-11 19:57:58 +0800

    :chart_with_upwards_trend: package main

--- utils/request/lib/index.js
+++ utils/request/lib/index.js
@@ -1,10 +1,20 @@
 const axios = require('axios')
 
-const BASE_URL = 'http://www.sunny.com:70001'
+const BASE_URL = 'http://www.sunny.com:7001'
 
 const instance = axios.create({
     baseURL: BASE_URL,
     timeout: 5000
 })
+
+instance.interceptors.response.use(
+    response => {
+        if (response.status === 200) {
+            return response.data
+        }
+    }, error => {
+        return Promise.reject(error)
+    }
+)
 
 module.exports = instance
